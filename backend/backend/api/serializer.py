8eabf10fae69da9372db2de7389dc8e7081b4416
pendonj14 <pendon.josephjr@gmail.com>
2025-09-17 22:14:39 +0800

    completed backed based on the student request for credential form

--- backend/backend/api/serializer.py
+++ backend/backend/api/serializer.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from .models import StudentRequest 
+from .models import StudentRequest, UserProfile
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
 
 class StudentRequestSerializer(serializers.ModelSerializer):
+    user_name = serializers.SerializerMethodField()
+    birth_date = serializers.SerializerMethodField()
+    
     class Meta:
         model = StudentRequest
         fields = '__all__'
+        
+    def get_user_name(self, obj):
+        return str(obj.user.profile)
+        
+    def get_birth_date(self, obj):
+        return obj.user.profile.birth_date
