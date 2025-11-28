b53a26e4e365ad9978975b93e7ea3994221a8f29
pendonj14 <pendon.josephjr@gmail.com>
2025-11-28 18:25:35 +0800

    migrated to postgresql

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -96,8 +96,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'backend',
+        'USER': 'joseph',
+        'PASSWORD': 'joseph',
     }
 }
 
